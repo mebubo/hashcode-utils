9f57af9287fc7ad1be27ad39ae8dcd82816742a7
Joffrey BION <joffrey.bion@gmail.com>
2017-03-06 00:22:35 +0100

    Pass solver function as lambda

--- src/main/java/org/hildan/hashcode/utils/solver/HCSolver.java
+++ src/main/java/org/hildan/hashcode/utils/solver/HCSolver.java
@@ -1,37 +1,28 @@
 package org.hildan.hashcode.utils.solver;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.function.Function;
 
 import org.hildan.hashcode.utils.parser.HCParser;
 
-public abstract class HCSolver<P> extends AbstractFileSolver {
+public class HCSolver<P> extends AbstractFileSolver {
 
     private final HCParser<P> problemParser;
 
-    protected HCSolver(HCParser<P> problemParser) {
-        this.problemParser = problemParser;
-    }
+    private final Function<P, ? extends Iterable<? extends CharSequence>> solver;
 
-    public static <P> HCSolver<P> from(HCParser<P> problemParser, Function<P, List<String>> solver) {
-        return new HCSolver<P>(problemParser) {
-            @Override
-            protected List<String> solve(P problem) {
-                return solver.apply(problem);
-            }
-        };
+    protected HCSolver(HCParser<P> problemParser, Function<P, ? extends Iterable<? extends CharSequence>> solver) {
+        this.problemParser = problemParser;
+        this.solver = solver;
     }
 
     @Override
     protected Iterable<? extends CharSequence> solve(String inputFilename) {
         try {
             P problem = problemParser.parse(inputFilename);
-            return solve(problem);
+            return solver.apply(problem);
         } catch (IOException e) {
             throw new SolverException("Exception occurred while parsing the input file '" + inputFilename + "'", e);
         }
     }
-
-    protected abstract Iterable<? extends CharSequence> solve(P problem);
 }
--- src/test/java/org/hildan/hashcode/utils/solver/HCSolverTest.java
+++ src/test/java/org/hildan/hashcode/utils/solver/HCSolverTest.java
@@ -24,7 +24,7 @@ public class HCSolverTest {
     private static class Input {
         public int num;
 
-        public String[] elements;
+        public String[] items;
     }
 
     private static final String TEST_FILENAME = "testfile.in";
@@ -44,29 +44,25 @@ public class HCSolverTest {
 
     @Before
     public void setUp() throws IOException {
-        TreeObjectReader<Input> inputReader = TreeObjectReader.of(Input::new)
-                                                              .addFieldsLine("num")
-                                                              .addArrayLine((i, s) -> i.elements = s, String[]::new,
-                                                                      s -> s);
+        TreeObjectReader<Input> inputReader = TreeObjectReader.of(Input::new) //
+                                                              .addFieldsLine("num") //
+                                                              .addStringArrayLine((i, s) -> i.items = s);
         HCParser<Input> parser = new HCParser<>(inputReader);
-        solver = HCSolver.from(parser, i -> Arrays.stream(i.elements).map(s -> s + i.num).collect(Collectors.toList()));
+        solver = new HCSolver<>(parser, i -> Arrays.stream(i.items).map(s -> s + i.num).collect(Collectors.toList()));
     }
 
     @Test
     public void solve_success() {
-        Input input = new Input();
-        input.num = 5;
-        input.elements = new String[]{"A", "B", "C"};
         @SuppressWarnings("unchecked")
-        List<String> lines = (List<String>) solver.solve(input);
+        List<String> lines = (List<String>) solver.solve(TEST_FILENAME);
         assertEquals(3, lines.size());
-        assertEquals("A5", lines.get(0));
-        assertEquals("B5", lines.get(1));
-        assertEquals("C5", lines.get(2));
+        assertEquals("abc42", lines.get(0));
+        assertEquals("def42", lines.get(1));
+        assertEquals("ghi42", lines.get(2));
     }
 
     @Test
-    public void fullTest() throws IOException {
+    public void accept_success() throws IOException {
         solver.accept(TEST_FILENAME);
         Path outputFilePath = Paths.get(EXPECTED_OUTPUT_FILENAME);
         assertTrue(Files.exists(outputFilePath));
