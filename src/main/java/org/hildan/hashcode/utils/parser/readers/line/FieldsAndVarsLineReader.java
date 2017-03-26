d637cb5ff750b96f6de45455e0d4bcb35b10d300
Joffrey BION <joffrey.bion@gmail.com>
2017-03-26 17:00:24 +0200

    Make FieldsAndVarsLineReader use FieldAndVarReaders

--- src/main/java/org/hildan/hashcode/utils/parser/readers/line/FieldsAndVarsLineReader.java
+++ src/main/java/org/hildan/hashcode/utils/parser/readers/line/FieldsAndVarsLineReader.java
@@ -1,12 +1,11 @@
 package org.hildan.hashcode.utils.parser.readers.line;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hildan.hashcode.utils.parser.InputParsingException;
 import org.hildan.hashcode.utils.parser.context.Context;
-import org.hildan.hashcode.utils.parser.conversion.StringConversionException;
-import org.hildan.hashcode.utils.parser.conversion.StringConverter;
+import org.hildan.hashcode.utils.parser.readers.section.FieldAndVarReader;
 import org.hildan.hashcode.utils.parser.readers.section.SectionReader;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,41 +15,29 @@ import org.jetbrains.annotations.NotNull;
  * @param <P>
  *         the type of parent that this {@code FieldsAndVarsLineReader} can update
  */
-public class FieldsAndVarsLineReader<P> extends LineReader<String[]> implements SectionReader<P> {
+public class FieldsAndVarsLineReader<P> implements SectionReader<P> {
 
-    private final String[] fieldAndVarNames;
-
-    private final String[] fields;
-
-    private final String[] variables;
+    private final List<FieldAndVarReader<P>> readers;
 
     /**
      * Creates a new {@code FieldsAndVarsLineReader} with the given field/variable names.
      * <p>
-     * The field/variable names are given as strings that can each be one of:
-     * <ul>
-     * <li>a field name (e.g. "myField1")</li>
-     * <li>a '@' symbol followed by a variable name (e.g. "@N", "@myVar", "@123"...)</li>
-     * <li>both a field name and a variable name separated by a '@' (e.g. "nItems@N", "size@nbOfSatellites"...)</li>
-     * </ul>
+     * The field/variable names are given as strings that can each be one of: <ul> <li>a field name (e.g.
+     * "myField1")</li> <li>a '@' symbol followed by a variable name (e.g. "@N", "@myVar", "@123"...)</li> <li>both a
+     * field name and a variable name separated by a '@' (e.g. "nItems@N", "size@nbOfSatellites"...)</li> </ul>
      * <p>
-     * Note that "" and "@" describe neither a field nor a variable, and thus the corresponding entry in the line will
-     * be ignored during parsing. A null description is forbidden.
+     * Note that "" describe neither a field nor a variable, and thus the corresponding entry in the line will be
+     * ignored during parsing. Null descriptions and descriptions ending in '@' are forbidden.
      *
      * @param fieldAndVarNames
      *         an array of field/variable names, as described above
      */
     public FieldsAndVarsLineReader(String... fieldAndVarNames) {
-        super(arr -> arr);
-        this.fieldAndVarNames = fieldAndVarNames;
-        this.fields = new String[fieldAndVarNames.length];
-        this.variables = new String[fieldAndVarNames.length];
+        this.readers = new ArrayList<>(fieldAndVarNames.length);
 
-        for (int i = 0; i < fieldAndVarNames.length; i++) {
-            String name = fieldAndVarNames[i];
+        for (String name : fieldAndVarNames) {
             String[] fieldAndVar = splitFieldAndVar(name);
-            fields[i] = fieldAndVar[0];
-            variables[i] = fieldAndVar[1];
+            readers.add(new FieldAndVarReader<>(fieldAndVar[0], fieldAndVar[1]));
         }
     }
 
@@ -59,54 +46,29 @@ public class FieldsAndVarsLineReader<P> extends LineReader<String[]> implements 
         if (name == null) {
             throw new IllegalArgumentException("Null field/variable descriptions are not allowed");
         }
-        if (!name.contains("@")) {
-            return new String[]{name, ""};
+        if (name.isEmpty()) {
+            return new String[]{null, null};
         }
-        return name.split("@", 2);
+        if (!name.contains("@")) {
+            return new String[]{name, null};
+        }
+        String[] fieldAndVar = name.split("@", 2);
+        nullifyEmptyStrings(fieldAndVar);
+        return fieldAndVar;
+    }
+
+    private static void nullifyEmptyStrings(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[0] != null && array[0].isEmpty()) {
+                array[0] = null;
+            }
+        }
     }
 
     @Override
     public void readAndSet(@NotNull Context context, @NotNull P parent) throws InputParsingException {
-        String[] values = read(context, parent);
-
-        if (values.length != fieldAndVarNames.length) {
-            throw new InputParsingException(
-                    "The number of values doesn't match the expected fields: " + Arrays.toString(fieldAndVarNames));
-        }
-        for (int i = 0; i < fieldAndVarNames.length; i++) {
-            if (!variables[i].isEmpty()) {
-                context.setVariable(variables[i], values[i]);
-            }
-            if (!fields[i].isEmpty()) {
-                setField(parent, fields[i], values[i]);
-            }
-        }
-    }
-
-    private static void setField(Object obj, String fieldName, String value) throws InputParsingException {
-        Class<?> clazz = obj.getClass();
-        try {
-            Field field = clazz.getDeclaredField(fieldName);
-            setField(obj, field, value);
-        } catch (NoSuchFieldException e) {
-            throw new InputParsingException(
-                    String.format("The provided field name '%s' was not found in class '%s'", fieldName,
-                            clazz.getSimpleName()), e);
-        }
-    }
-
-    private static void setField(Object obj, Field field, String value) throws InputParsingException {
-        try {
-            field.setAccessible(true);
-            field.set(obj, StringConverter.convert(field.getType(), value));
-        } catch (StringConversionException e) {
-            throw new InputParsingException(
-                    "Type mismatch, cannot assign value '" + value + "' to field '" + field.getName() + "' of type "
-                            + field.getType().getSimpleName());
-        } catch (IllegalAccessException e) {
-            throw new InputParsingException(
-                    "Could not set field '" + field.getDeclaringClass().getSimpleName() + "." + field.getName()
-                            + "' to value '" + value + "'", e);
+        for (FieldAndVarReader<P> reader : readers) {
+            reader.readAndSet(context, parent);
         }
     }
 }
